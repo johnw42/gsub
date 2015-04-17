4faad0229b2fa1cf24a445a905cb72cd4ea26185
John Williams <jrw@pobox.com>
2015-04-17 10:03:16 -0700

    added unit tests for transformLineFixed

--- test/GsubTest.hs
+++ test/GsubTest.hs
@@ -6,6 +6,7 @@ import Plan
 import PlanTest ()
 
 import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Char (toLower, toUpper)
 import Data.List (isInfixOf)
 import Data.Maybe (isNothing)
 import Test.Framework (testGroup)
@@ -16,6 +17,30 @@ import Test.QuickCheck
 
 instance Show Plan where
     show p = "Plan {options = " ++ show (options p) ++ "}"
+
+instance Arbitrary CaseHandling where
+    arbitrary = elements [IgnoreCase, ConsiderCase]
+
+prop_transformLineFixed1 ch pattern replacement before after =
+    not (pattern `isInfixOf` (replacement ++ after)) ==>
+    not (pattern `isInfixOf` (before ++ replacement)) ==>
+    printTestCase (show rkesult) $
+        replacement `isInfixOf` result &&
+        not (pattern `isInfixOf` result)
+  where
+    content = before ++ pattern ++ after
+    result = transformLineFixed ch pattern replacement content
+
+prop_transformLineFixed2 pattern replacement before after =
+    not (u pattern `isInfixOf` (u $ replacement ++ after)) ==>
+    not (u pattern `isInfixOf` (u $ before ++ replacement)) ==>
+    printTestCase (show result) $
+        replacement `isInfixOf` result &&
+        not (pattern `isInfixOf` result)
+  where
+    content = before ++ pattern ++ after
+    u = map toUpper
+    result = transformLineFixed IgnoreCase (u pattern) replacement content
 
 prop_transformLine plan before after =
     useFixedStrings plan ==>  -- XXX
@@ -45,6 +70,8 @@ prop_transformFileContent plan before after =
           result' = L8.unpack result
 
 tests = testGroup "Gsub" [
+  testProperty "transformLineFixed1" prop_transformLineFixed1,
+  testProperty "transformLineFixed2" prop_transformLineFixed2,
   testProperty "transformLine(XXX)" prop_transformLine,
   testProperty "transformFileContent(XXX)" prop_transformFileContent
   ]
